9eb90b523fa3bf9be848fe8337fdfcc38f72d899
Jaehoon Oh <ojh420@gmail.com>
2023-08-30 07:26:59 +0900

    test: add a game update test

--- index.ts
+++ index.ts
@@ -110,6 +110,8 @@ export function update() {
       }
     }
   }
+
+  return map;
 }
 
 function draw() {
--- tests/gatherer.test.ts
+++ tests/gatherer.test.ts
@@ -1,7 +1,20 @@
 import { update } from "../index"
 
 describe('Game', function() {
-  it('First2', function() {
-    update();
+  it('update test', function() {
+    const expectedMap = [
+        [ 2, 2, 2, 2, 2, 2, 2, 2 ],
+        [ 2, 3, 0, 1, 1, 2, 0, 2 ],
+        [ 2, 4, 2, 6, 1, 2, 0, 2 ],
+        [ 2, 8, 4, 1, 1, 2, 0, 2 ],
+        [ 2, 4, 1, 1, 1, 9, 0, 2 ],
+        [ 2, 2, 2, 2, 2, 2, 2, 2 ]
+      ];
+
+    const resultMap = update();
+
+    console.log("resultMap=",resultMap);
+
+    expect(resultMap).toEqual(expectedMap);
   });
 })
